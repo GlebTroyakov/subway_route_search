57a745fd3485d839c41f21768529bbfb3586c16e
GlebTroyakov <glebtroyakov@gmail.com>
2022-10-27 18:44:46 +0300

    Добавление методов в класс Link
     - добавлен __init__
    
     - добавлен __eq__ для сравнения связей
    
     - добавлены геттер v1 и v2
    
     - добавлен геттер и сеттер dist

--- subway_route_search.py
+++ subway_route_search.py
@@ -12,7 +12,34 @@ class Vertex:
 
 
 class Link:
-    pass
+    def __init__(self, v1: Vertex, v2: Vertex):
+        self._v1 = v1
+        self._v2 = v2
+        self._dist = 1  # длина связи (по умолчанию 1); это может быть длина пути, время в пути и др.
+
+    def __eq__(self, other):
+        assert isinstance(other, Link), 'Сравневается не связь'
+        if self._v1 == other._v1 and self._v2 == other._v2:
+            return True
+        if self._v1 == other._v2 and self._v2 == other._v1:
+            return True
+        return False
+
+    @property
+    def v1(self):
+        return self._v1
+
+    @property
+    def v2(self):
+        return self._v2
+
+    @property
+    def dist(self):
+        return self._dist
+
+    @dist.setter
+    def dist(self, value):
+        self._dist = value
 
 
 class LinkedGraph:
